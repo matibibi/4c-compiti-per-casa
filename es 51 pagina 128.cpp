9d234f47fd004cd3e8991c1824c8310f8eb7a2a0
matibibi <matisly@hotmail.it>
2016-11-21 15:06:27 +0100

    Add files via upload

---
+++ es 51 pagina 128.cpp
@@ -0,0 +1,15 @@
+//Es 51 pagina 128
+//Dopo aver caricato in memoria un array di numeri interi con 10 componenti, contare le componenti che hanno valore superiore a 5.
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int NumeriInteri [10];
+
+    for (int i=5; i<=10; i++)
+        cout<< NumeriInteri[i] <<endl;
+
+    return 0;
+}
